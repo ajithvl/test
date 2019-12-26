912dc0b7f1e04eaf41f8378e036916420648ff87
Ajith Looka <ajith@headspin.io>
2019-12-26 14:38:12 -0800

    Initial check-in of module HeadspinDemo

---
+++ UnitTest1.cs
@@ -0,0 +1,29 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace HeadspinDemo
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void SearchForCheese()
+        {
+            //using (var driver = new ChromeDriver()
+            //{
+            //    driver.Navigate().GoToUrl("http://www.google.com")
+            //}
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("headless");
+            ChromeDriver driver = new ChromeDriver(chromeOptions);
+            driver.Navigate().GoToUrl("http://www.google.com");
+
+            driver.Quit();
+        }
+    }
+}
